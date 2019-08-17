df0db059a99ba5c01e1650c1c9505bc78f9caa26
Hansel-Christopher <chris23han@gmail.com>
2019-08-17 12:16:59 +0530

    adding func to scrap with pid from local db

--- server/app.js
+++ server/app.js
@@ -1,1 +1,41 @@
-console.log('dev server started');
+var request = require('request');
+var cheerio = require('cheerio');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://pricehound:admin123@localhost:27017/";
+var pid = "";
+MongoClient.connect(url, { useNewUrlParser: true }  ,function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("pricehound");
+
+  console.log("database connected");
+  dbo.collection("pricehound").findOne({}, function(err, result) {
+    if (err) throw err;
+    console.log(result.Product_id_flipkart);
+    pid = result.Product_id_flipkart;
+    db.close();
+  });
+});
+
+console.log('scrapper started');
+
+function getWarranty(id) {
+    request.get(`https://www.flipkart.com/whirlpool-200-l-direct-cool-single-door-4-star-refrigerator-base-drawer/p/itmevysxpc7hkuhd?pid=${id}`, function(
+      error,
+      response,
+      data
+    ) {
+      const $ = cheerio.load(data);
+      var war =  $("._3h7IGd")  
+            .text()
+    .trim();
+      var warranty = war.substring(0, war.length-9);
+      console.log(warranty);
+    });
+  }
+
+  getWarranty(pid);
+
+
+// setInterval(getWarranty, 1500, "RFREVYSXE2YJJHSG");
+
+  
