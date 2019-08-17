bc932c44f0bb8117017b894ea90ee324b7b0ed9f
Hansel-Christopher <chris23han@gmail.com>
2019-08-18 01:42:16 +0530

    removed db, accessing pids from strapi && should debug the put request

--- server/app.js
+++ server/app.js
@@ -1,25 +1,30 @@
 var request = require('request');
 var cheerio = require('cheerio');
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://pricehound:admin123@localhost:27017/";
+const axios = require('axios');
+const url = require('url');
+const querystring = require('query-string');
 var pid = "";
-MongoClient.connect(url, { useNewUrlParser: true }  ,function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("pricehound");
+var data;
+console.log('scraper started');
 
-  console.log("database connected");
-  dbo.collection("pricehound").findOne({}, function(err, result) {
-    if (err) throw err;
-    console.log(result.Product_id_flipkart);
-    pid = result.Product_id_flipkart;
-    db.close();
-  });
-});
+var config = {
+    headers: {'X-My-Custom-Header': 'Header-Value'}
+  };
 
-console.log('scrapper started');
+ axios.get(
+        'http://ec2-13-233-84-193.ap-south-1.compute.amazonaws.com:1337/refrigerators/',
+      ).then(function(response){
+    
+      data = response.data;
+      getWarranty(data[0].product_id_flipkart, data[0]._id);
+      
 
-function getWarranty(id) {
-    request.get(`https://www.flipkart.com/whirlpool-200-l-direct-cool-single-door-4-star-refrigerator-base-drawer/p/itmevysxpc7hkuhd?pid=${id}`, function(
+    //   data.forEach((element)=>{
+    //       getWarranty(element.product_id_flipkart);
+    //   })
+      });
+function getWarranty(id, fid) {
+    request.get(`https://www.flipkart.com/test/p/test?pid=${id}`, function(
       error,
       response,
       data
@@ -28,14 +33,24 @@ function getWarranty(id) {
       var war =  $("._3h7IGd")  
             .text()
     .trim();
+      var rate = $("._1i0wk8").text().trim();
+      var rc = $(".row _2yc1Qo > .col-12-12").text().trim();
+      var rcount = rc.substring(0,rc.length-9);
       var warranty = war.substring(0, war.length-9);
+
+      console.log(rcount);
       console.log(warranty);
+      console.log(rate);
+      console.log(typeof(fid));
+      axios.post(
+        "http://ec2-13-233-84-193.ap-south-1.compute.amazonaws.com:1337/refrigerators/5d5626414be5372232eefe7f", 
+        querystring.stringify({ warranty: warranty })
+        ).then(r => console.log(r))
+        .catch(e => console.log(e));
     });
   }
 
   getWarranty(pid);
-
-
 // setInterval(getWarranty, 1500, "RFREVYSXE2YJJHSG");
 
   
